e30a155ece5d46e6976be27a117797313ece20b4
Beki <uzbekitube@gmail.com>
2025-04-26 21:47:57 +0900

    update script updated to update static files and run migrations

--- update.sh
+++ update.sh
@@ -1,4 +1,8 @@
 # update script that git pulls, restarts gunicorn server 'corpus' and restarts nginx
-git pull
+git pull origin main
+. ./venv/bin/activate
+pip install --upgrade -r requirements.txt
+python3 manage.py collectstatic --noinput
+python3 manage.py migrate
 sudo systemctl restart corpus
-sudo systemctl restart nginx
+sudo nginx -s reload
