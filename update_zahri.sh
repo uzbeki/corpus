31181ef3bdefe542a5166d526550542bc3694083
Beki <uzbekitube@gmail.com>
2026-01-18 13:06:59 +0500

    venv updated

--- update_zahri.sh
+++ update_zahri.sh
@@ -1,6 +1,6 @@
 # update script that git pulls, restarts gunicorn server 'zahri' and restarts nginx
 git pull origin zahri
-. ./venv/bin/activate
+. ./.venv/bin/activate
 pip install --upgrade -r requirements.txt
 python3 manage.py collectstatic --noinput
 python3 manage.py migrate
