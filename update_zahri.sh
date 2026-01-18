0cab854e95d0f2d98c4540c3800899d540fb9872
Beki <uzbekitube@gmail.com>
2026-01-18 13:06:04 +0500

    update scripts added

--- update_zahri.sh
+++ update_zahri.sh
@@ -1,8 +1,8 @@
-# update script that git pulls, restarts gunicorn server 'corpus' and restarts nginx
-git pull origin main
+# update script that git pulls, restarts gunicorn server 'zahri' and restarts nginx
+git pull origin zahri
 . ./venv/bin/activate
 pip install --upgrade -r requirements.txt
 python3 manage.py collectstatic --noinput
 python3 manage.py migrate
-sudo systemctl restart corpus
+sudo systemctl restart zahri
 sudo nginx -s reload
